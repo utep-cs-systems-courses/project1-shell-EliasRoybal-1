8ab4fd2d379b2e291c198641e292eebc22e1d4e2
EliasRoybal <eroybal@miners.utep.edu>
2020-09-08 03:44:14 -0600

    Simple shell created to do minor things such as printing and doing basic math

---
+++ shell/shell.py
@@ -0,0 +1,12 @@
+while 1:
+    userIn =input("$ ")
+    if userIn == "exit":
+        break
+    try:
+        command = eval(userIn)
+        if command: print(command)
+    except:
+        try:
+            exec(userIn)
+        except Exception as e:
+            print("Error: ", e)
