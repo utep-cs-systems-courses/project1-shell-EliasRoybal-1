92217f8a4cf115b055d6a3801e7b3df3bf7bb2e2
EliasRoybal <eroybal@miners.utep.edu>
2020-09-15 03:55:47 -0600

    attempted to set PS1 and added default command dict

--- shell/shell.py
+++ shell/shell.py
@@ -3,11 +3,17 @@ import sys
 import re
 
 processID = os.getpid()
+defaultCommands =['cd','exit']
 def initialPrompt():
-    while 1:
-        userIn =input("$ ")
+    defaultPrompt = '$ '
+    if 'PS1'in os.environ:
+        print("in if")
+        defaultPrompt = os.environ['PS1'] #makes the default prompt contain the default environment variable
+        
+    while True:
+        userIn =input(defaultPrompt)
         if userIn == "exit":
-            break
+            sys.exit(0)
         try:
             command = eval(userIn)
             fork =  os.fork()
@@ -17,6 +23,8 @@ def initialPrompt():
                 print("Child process ID:", processID)
             #print(processID)
             if command: print(command)
+        except KeyError as e:
+            print("Error: ", e)
         except:
             try:
                 exec(userIn)
@@ -24,3 +32,4 @@ def initialPrompt():
                 print("Error: ", e)
                 
 initialPrompt()
+        
