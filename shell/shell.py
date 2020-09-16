a74c8a07866082e0fbb40cb1833c45e31f03560d
EliasRoybal <eroybal@miners.utep.edu>
2020-09-15 22:42:02 -0600

    Finding directories works but other commands do not

--- shell/shell.py
+++ shell/shell.py
@@ -26,7 +26,38 @@ def initialPrompt():
             except FileNotFoundError:
                 os.write(2, ("No such directory found\n").encode()) #standard error file descriptor
 
-        elif
+        elif "|" in args:
+            fork = os.fork()
+            if fork == 0: #child process
+                pipe(args)
+            elif fork < 0:
+                os.write(2, ("Fork failed\n").encode())
+                sys.exit(1)
+            else: #parent for was good
+                if args[-1] != "&":
+                    val = os.wait()
+                    if val[1] != 0 and val[1] != 256:
+                        os.write(2, ("Program ended. Exit code: %d\n" % val[1].encode()))
+        else:
+            rc = os.fork()
+
+            isWaiting = True
+
+            if "&" in args:
+                isWaiting = False
+                args.remove("&")
+            if rc == 0:
+                redirectionAndExecution(args)
+            elif rc < 0:
+                os.write(2,("Fork failed".encode()))
+                sys.exit(1)
+
+            else:
+                if isWaiting:
+                    val = os.wait()
+                    if val[1] !=0 and val[1] != 256:
+                        os.write(2,("Program terminated. Exit Code: %d\n" % val[1]).encode())
+                        
 def pipe(args):
     write = args[0:args.index("|")]
     read = args[args.index("|")+1:]
@@ -41,17 +72,18 @@ def pipe(args):
         if "|" in read:
             pipe(read)
         initialPrompt() #runs going back to initialPrompt
-        os.write(2, ("Not executable").encode())
+        os.write(2, ("Not executable\n").encode())
+        sys.exit(1)
     elif rc == 0: #child process
         os.close(1) #closes output file descriptor
         os.dup(pipeWriter,1) #dups pipe writer to file descriptor 1
         for fileDescriptor in (pipeReader, pipeWriter):
             os.close(fileDescriptor) #closes both pipe reader and pipe writer
         initialPrompt() #goes back to initialPrompt
-        os.write(2,("Not executable").encode())
+        os.write(2,("Not executable\n").encode())
         sys.exit(1)
     else: #fork failed
-        os.write(2,("Fork failed").encode())
+        os.write(2,("Fork failed\n").encode())
         sys.exit(1)
 
 def redirectionAndExecution(args):
@@ -70,7 +102,7 @@ def redirectionAndExecution(args):
             args.remove(args[args.index('>')+1]) #removes file name
             args.remove('>') # removes >
     except IndexError:
-        os.write(2, "Not able to redirect")
+        os.write(2, "Not able to redirect\n")
     try:
         if args[0][0] == '/':
             os.execve(args[0],args,os.environ) #tries to run process
@@ -79,8 +111,7 @@ def redirectionAndExecution(args):
     except Exception:
         sys.exit(1)
 
-    os.write(2,("Command not found"))
+    os.write(2,("Command not found\n").encode())
     sys.exit(1)
-                        
-                        
+    
 initialPrompt()
